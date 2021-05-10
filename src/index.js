7f0915d2d073893aa6716d87f7d96c647730016d
gcmartin <gscalica@gmail.com>
2021-05-10 15:00:33 -0700

    Create Redux store and connect it to the App

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import App from './App';
 import "./assets/styles/index.css";
 import reportWebVitals from './reportWebVitals';
 
+// imports for the Redux store
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+// Create the Redux store
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
