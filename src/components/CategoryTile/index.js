72462cd69cc4b126f3efe5a6fb8dbcb3c065bea4
gcmartin <gscalica@gmail.com>
2021-05-13 15:12:46 -0700

    Style CategoryTile component

--- src/components/CategoryTile/index.js
+++ src/components/CategoryTile/index.js
@@ -6,9 +6,10 @@ function CategoryTile() {
   return (
 
     <Card className="CategoryTile">
-      <Card.Body>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Title>Card Title</Card.Title>
+      <Card.Body className="CategoryTile-body">
+        {/* Photo by Dan Gold: https://unsplash.com/@danielcgold */}
+        <Card.Img variant="top" src="https://images.unsplash.com/photo-1466978913421-dad2ebd01d17?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80" />
+        <Card.Title className="CategoryTile-title">Card Title</Card.Title>
       </Card.Body>
     </Card>
   );
