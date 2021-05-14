66e655ad7f544e9c6c33ed373f436ba720323f43
gcmartin <gscalica@gmail.com>
2021-05-14 16:38:35 -0700

    Re-style CategoryTile component

--- src/components/BestBusinessSection/index.js
+++ src/components/BestBusinessSection/index.js
@@ -8,24 +8,32 @@ function BestBusinessSection() {
       <h5 className="BestBusinessSection-title">Find the Best Businesses in Town</h5>
 
       {/* Photo by Jessica Ruscello: https://unsplash.com/@jruscello */}
-      <a href="#" className="BestBusinessSection-tile">
-        <CategoryTile src="https://images.unsplash.com/photo-1485322551133-3a4c27a9d925?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80" category="Bookstores"/>
-      </a>
-
+      <div className="BestBusinessSection-tile">
+        <a href="#" className="BestBusinessSection-link">
+          <CategoryTile src="https://images.unsplash.com/photo-1485322551133-3a4c27a9d925?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80" category="Bookstores"/>
+        </a>
+      </div>
+      
       {/* Photo by Bianca Jordan: https://unsplash.com/@biancajordan */}
-      <a href="#" className="BestBusinessSection-tile">
-        <CategoryTile src="https://images.unsplash.com/photo-1521656693074-0ef32e80a5d5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80" category="Laundromats"/>
-      </a>
+      <div className="BestBusinessSection-tile">
+        <a href="#" className="BestBusinessSection-link">
+          <CategoryTile src="https://images.unsplash.com/photo-1521656693074-0ef32e80a5d5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80" category="Laundromats"/>
+        </a>
+      </div>
 
       {/* Photo by Ronnie George: https://unsplash.com/@realestateron */}
-      <a href="#" className="BestBusinessSection-tile">
-        <CategoryTile src="https://images.unsplash.com/photo-1592595896551-12b371d546d5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80" category="Realtors"/>
-      </a>
+      <div className="BestBusinessSection-tile">
+        <a href="#" className="BestBusinessSection-link">
+          <CategoryTile src="https://images.unsplash.com/photo-1592595896551-12b371d546d5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80" category="Realtors"/>
+        </a>
+      </div>
 
       {/* Photo by HUUM: https://unsplash.com/@huumsauna */}
-      <a href="#" className="BestBusinessSection-tile">
-        <CategoryTile src="https://images.unsplash.com/photo-1583416750470-965b2707b355?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80" category="Spas"/>
-      </a>
+      <div className="BestBusinessSection-tile">
+        <a href="#" className="BestBusinessSection-link">
+          <CategoryTile src="https://images.unsplash.com/photo-1583416750470-965b2707b355?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80" category="Spas"/>
+        </a>
+      </div>
     </section>
   );
 }
