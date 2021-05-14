523310ef0bf47fe6bf0fa0d4ee043259ffe7bc54
gcmartin <gscalica@gmail.com>
2021-05-14 14:27:50 -0700

    Style BestBusinessSection and add bottom margin to tiles

--- src/components/BestBusinessSection/index.js
+++ src/components/BestBusinessSection/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import CategoryTile from "./../CategoryTile";
+import "./styles.css";
 
 function BestBusinessSection() {
   return (
-    <section>
-      <h2>Find the Best Businesses in Town</h2>
+    <section className="BestBusinessSection">
+      <h5 className="BestBusinessSection-title">Find the Best Businesses in Town</h5>
 
       {/* Photo by Jessica Ruscello: https://unsplash.com/@jruscello */}
       <CategoryTile src="https://images.unsplash.com/photo-1485322551133-3a4c27a9d925?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80" category="Bookstores"/>
