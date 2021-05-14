5cfa748ad20cb96e23af6b9c416f88a34ebe4c10
gcmartin <gscalica@gmail.com>
2021-05-14 14:10:05 -0700

    Add props to CategoryTile, resize image, and change padding for title

--- src/components/BestBusinessSection/index.js
+++ src/components/BestBusinessSection/index.js
@@ -5,7 +5,18 @@ function BestBusinessSection() {
   return (
     <section>
       <h2>Find the Best Businesses in Town</h2>
-        <CategoryTile />
+
+      {/* Photo by Jessica Ruscello: https://unsplash.com/@jruscello */}
+      <CategoryTile src="https://images.unsplash.com/photo-1485322551133-3a4c27a9d925?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80" category="Bookstores"/>
+      
+      {/* Photo by Bianca Jordan: https://unsplash.com/@biancajordan */}
+      <CategoryTile src="https://images.unsplash.com/photo-1521656693074-0ef32e80a5d5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80" category="Laundromats"/>
+      
+      {/* Photo by Ronnie George: https://unsplash.com/@realestateron */}
+      <CategoryTile src="https://images.unsplash.com/photo-1592595896551-12b371d546d5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80" category="Realtors"/>
+      
+      {/* Photo by HUUM: https://unsplash.com/@huumsauna */}
+      <CategoryTile src="https://images.unsplash.com/photo-1583416750470-965b2707b355?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80" category="Spas"/>
     </section>
   );
 }
--- src/components/CategoryTile/index.js
+++ src/components/CategoryTile/index.js
@@ -2,14 +2,12 @@ import React from "react";
 import { Card } from "react-bootstrap";
 import "./styles.css";
 
-function CategoryTile() {
+function CategoryTile(props) {
   return (
-
     <Card className="CategoryTile">
       <Card.Body className="CategoryTile-body">
-        {/* Photo by Dan Gold: https://unsplash.com/@danielcgold */}
-        <Card.Img variant="top" src="https://images.unsplash.com/photo-1466978913421-dad2ebd01d17?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80" />
-        <Card.Title className="CategoryTile-title">Card Title</Card.Title>
+        <Card.Img variant="top" className="CategoryTile-img" src={props.src} />
+        <Card.Title className="CategoryTile-title">{props.category}</Card.Title>
       </Card.Body>
     </Card>
   );
