95e44ec727a1a906514c962f61c5b63059715f60
gcmartin <gscalica@gmail.com>
2021-05-14 15:31:00 -0700

    Change tiles into links and add hover effect

--- src/components/BestBusinessSection/index.js
+++ src/components/BestBusinessSection/index.js
@@ -8,16 +8,24 @@ function BestBusinessSection() {
       <h5 className="BestBusinessSection-title">Find the Best Businesses in Town</h5>
 
       {/* Photo by Jessica Ruscello: https://unsplash.com/@jruscello */}
-      <CategoryTile src="https://images.unsplash.com/photo-1485322551133-3a4c27a9d925?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80" category="Bookstores"/>
-      
+      <a href="#" className="BestBusinessSection-tile">
+        <CategoryTile src="https://images.unsplash.com/photo-1485322551133-3a4c27a9d925?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80" category="Bookstores"/>
+      </a>
+
       {/* Photo by Bianca Jordan: https://unsplash.com/@biancajordan */}
-      <CategoryTile src="https://images.unsplash.com/photo-1521656693074-0ef32e80a5d5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80" category="Laundromats"/>
-      
+      <a href="#" className="BestBusinessSection-tile">
+        <CategoryTile src="https://images.unsplash.com/photo-1521656693074-0ef32e80a5d5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80" category="Laundromats"/>
+      </a>
+
       {/* Photo by Ronnie George: https://unsplash.com/@realestateron */}
-      <CategoryTile src="https://images.unsplash.com/photo-1592595896551-12b371d546d5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80" category="Realtors"/>
-      
+      <a href="#" className="BestBusinessSection-tile">
+        <CategoryTile src="https://images.unsplash.com/photo-1592595896551-12b371d546d5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80" category="Realtors"/>
+      </a>
+
       {/* Photo by HUUM: https://unsplash.com/@huumsauna */}
-      <CategoryTile src="https://images.unsplash.com/photo-1583416750470-965b2707b355?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80" category="Spas"/>
+      <a href="#" className="BestBusinessSection-tile">
+        <CategoryTile src="https://images.unsplash.com/photo-1583416750470-965b2707b355?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80" category="Spas"/>
+      </a>
     </section>
   );
 }
