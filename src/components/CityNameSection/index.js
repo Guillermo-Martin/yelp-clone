c79a7cfeda948af252d8eaa24980972ddfc31032
gcmartin <gscalica@gmail.com>
2021-05-17 15:50:44 -0700

    Style 'Hot & New Businesses' portion and cards

--- src/components/CityNameSection/index.js
+++ src/components/CityNameSection/index.js
@@ -5,19 +5,15 @@ import "./styles.css";
 function CityNameSection() {
   return (
     <section className="CityNameSection">
-      {/* NEXT: Style */}
       <h5 className="CityNameSection-title">Yelp Oakland</h5>
 
       {/* City Navbar */}
       <ul className="CityNameSection-ul"> 
-        {/* NEXT: convert to links */}
-        {/* NEXT: style */}
         <li><a href="#" className="CityNameSection-links">Alameda</a></li>
         <li><a href="#" className="CityNameSection-links">Oakland</a></li>
         <li><a href="#" className="CityNameSection-links">San Francisco</a></li>
         <li><a href="#" className="CityNameSection-links">Berkeley</a></li>
         <li><a href="#" className="CityNameSection-links">Emeryville</a></li>
-        {/* NEXT: add magnifying glass icon */}
         <li><a href="#" className="CityNameSection-links"><i class="fas fa-search CityNameSection-search"></i> More Cities</a></li>
       </ul>
 
@@ -25,16 +21,42 @@ function CityNameSection() {
 
       <h6>Hot &#38; New Businesses</h6>
       {/* InfoCards */}
-      <InfoCard 
-        src="https://images.unsplash.com/photo-1607290817806-e93c813ff329?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80"
-        name="Pizza"
-        review="**** 10 reviews"
-        type="Pizza, Wings"
-        city="Oakland, CA"
-        weeks="2"
-      />
+      <div className="CityNameSection-card">
+        {/* Image by Linda Hinton: https://unsplash.com/@lyndaann1975 */}
+        <InfoCard 
+          src="https://images.unsplash.com/photo-1607290817806-e93c813ff329?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80"
+          name="Pizza"
+          review="**** 10 reviews"
+          type="Pizza, Wings"
+          city="Oakland, CA"
+          weeks="2"
+        />
+      </div>
+      
+      <div className="CityNameSection-card">
+        {/* Image by Mike:  https://unsplash.com/@omaha */}
+        <InfoCard 
+          src="https://images.unsplash.com/photo-1594212699903-ec8a3eca50f5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3451&q=80"
+          name="Burgers"
+          review="**** 12 reviews"
+          type="Burgers, American (Traditional)"
+          city="Berkeley, CA"
+          weeks="3"
+        />
+      </div>
 
-      <p>See more hot and new businesses</p>
+      <div className="CityNameSection-card">
+        <InfoCard 
+          src="https://images.unsplash.com/photo-1611143669185-af224c5e3252?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3580&q=80"
+          name="Sushi"
+          review="**** 20 reviews"
+          type="Sushi Bars, Japanese"
+          city="San Francisco, CA"
+          weeks="1"
+        />
+      </div>
+      
+      <a href="#"><p className="CityNameSection-seeMore">See more hot and new businesses</p></a>
     </section>
 
   );
--- src/components/InfoCard/index.js
+++ src/components/InfoCard/index.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import "./styles.css"
 
 function InfoCard(props) {
   return (
-    <Card style={{ width: '18rem' }}>
+    /* width will take 100% of its container */
+    <Card className="InfoCard">
       {/* NEXT: convert image to a link */}
-      <Card.Img variant="top" src={props.src} />
+      <a href="#"><Card.Img variant="top" src={props.src} /></a>
       <Card.Body>
         {/* NEXT: convert name to link */}
-        <Card.Title>{props.name}</Card.Title>
+        <Card.Title className="InfoCard-title"><a href="#">{props.name}</a></Card.Title>
         {/* NEXT: Add image for stars */}
-        <Card.Text>{props.review}</Card.Text>
-        <Card.Text>{props.type}</Card.Text>
-        <Card.Text>{props.city}</Card.Text>
-        <Card.Text>Opened {props.weeks} weeks ago</Card.Text>
+        <Card.Text className="InfoCard-review">{props.review}</Card.Text>
+        <Card.Text className="InfoCard-text">{props.type}</Card.Text>
+        <Card.Text className="InfoCard-text">{props.city}</Card.Text>
+        <Card.Text className="InfoCard-open"><i class="fas fa-fire"></i> Opened {props.weeks} weeks ago</Card.Text>
       </Card.Body>
     </Card>
   );
