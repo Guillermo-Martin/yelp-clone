b6671b71df1f20ba613114daa6ee9354ab786c5d
gcmartin <gscalica@gmail.com>
2021-05-19 15:27:04 -0700

    Create DetailsPage component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 // import Counter from "./components/Counter";
 import HomePage from "./components/HomePage";
+import DetailsPage from "./components/DetailsPage";
 import TopNavbar from "./components/TopNavbar";
+
 
 function App() {
   return (
     <div>
-      <TopNavbar />
-      <HomePage />
+      {/* <TopNavbar /> */}
+      {/* <HomePage /> */}
+      <DetailsPage />
     </div>
   );
 }
---
+++ src/components/DetailsPage/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function DetailsPage() {
+  return (
+    <div>
+      <h1>Details Page!</h1>
+      {/* Hero Image with basic details section */}
+
+      {/* Basic info section (website, address, etc.) */}
+
+      {/* COVID-19 updates section (?) */}
+
+      {/* Location and hours section (map) */}
+
+      {/* Review section (with reviews) */}
+      
+    </div>
+  );
+}
+
+export default DetailsPage;
