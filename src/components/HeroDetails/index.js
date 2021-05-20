1b4ec10b835efb168a703f78e720d6fb2eca0e55
gcmartin <gscalica@gmail.com>
2021-05-20 14:52:45 -0700

    Position info for HeroDetails

--- src/components/HeroDetails/index.js
+++ src/components/HeroDetails/index.js
@@ -1,11 +1,14 @@
 import React from "react";
+import reviewStars from "./../../assets/images/reviewStars.png";
+import "./styles.css";
 
 function HeroDetails() {
   return (
-    <section>
+    <section className="HeroDetails">
       <div>
-        <h2>Business Name</h2>
-        <p>Reviews</p>
+        <h2 className="HeroDetails-name">Business Name</h2>
+        <img src={reviewStars} />
+        <p>10 reviews</p>
         <p>Claimed/Type of Business</p>
         <p>Hours</p>
       </div>
