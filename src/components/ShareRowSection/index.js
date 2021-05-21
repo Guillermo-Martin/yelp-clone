036a2dd877302d1a99a2dd4fe7de29336900704c
gcmartin <gscalica@gmail.com>
2021-05-21 14:04:46 -0700

    Style ShareRowSection and its buttons

--- src/components/ShareRowSection/index.js
+++ src/components/ShareRowSection/index.js
@@ -1,13 +1,33 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import "./styles.css";
 
 function ShareRowSection() {
   return (
-    <section>
-      <Button>Write a Review</Button>
-      <Button>Add Photo</Button>
-      <Button>Share</Button>
-      <Button>Save</Button>
+    <section className="ShareRowSection">
+      <a href="#">
+        <Button className="ShareRowSection-btn review">
+          <i class="far fa-star"></i>Write a Review
+        </Button>
+      </a>
+
+      <a href="#">
+        <Button className="ShareRowSection-btn other" variant="outline-secondary">
+          <i class="fas fa-camera"></i>Add Photo
+        </Button>
+      </a>
+
+      <a href="#">
+        <Button className="ShareRowSection-btn other" variant="outline-secondary">
+          <i class="fas fa-upload"></i>Share
+        </Button>
+      </a>
+
+      <a href="#">
+        <Button className="ShareRowSection-btn other" variant="outline-secondary">
+          <i class="far fa-bookmark"></i>Save
+        </Button>
+      </a>
     </section>
   );
 }
