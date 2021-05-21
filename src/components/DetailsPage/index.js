0867c63964a0a35e5dbbeafdb204d54c243eac03
gcmartin <gscalica@gmail.com>
2021-05-21 11:50:27 -0700

    Create ShareRowSection component

--- src/components/DetailsPage/index.js
+++ src/components/DetailsPage/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import HeroDetails from "./../HeroDetails";
 import BasicInfoSection from "./../BasicInfoSection";
+import ShareRowSection from "../ShareRowSection";
 
 function DetailsPage() {
   return (
@@ -8,6 +9,9 @@ function DetailsPage() {
       {/* <h1>Details Page!</h1> */}
       {/* Hero Image with basic details section */}
       <HeroDetails />
+
+      {/* List/Row of buttons (write a review, add photo, share, save) */}
+      <ShareRowSection />
 
       {/* Basic info section (website, address, etc.) */}
       <BasicInfoSection />
---
+++ src/components/ShareRowSection/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+function ShareRowSection() {
+  return (
+    <section>
+      <Button>Write a Review</Button>
+      <Button>Add Photo</Button>
+      <Button>Share</Button>
+      <Button>Save</Button>
+    </section>
+  );
+}
+
+export default ShareRowSection;
