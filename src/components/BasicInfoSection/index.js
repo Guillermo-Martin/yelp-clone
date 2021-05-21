9482d1db1f64568c036da12f30f3ff0dd0c64244
gcmartin <gscalica@gmail.com>
2021-05-21 11:38:09 -0700

    Style BasicInfoSection

--- src/components/BasicInfoSection/index.js
+++ src/components/BasicInfoSection/index.js
@@ -3,13 +3,33 @@ import "./styles.css";
 
 function BasicInfoSection() {
   return (
-    <section>
-      <div>
-        <h2>Basic Info</h2>
-        <p>Website</p>
-        <p>Phone Number</p>
-        <p>Address</p>
-        <p>Contact the Business</p>
+    <section className="BasicInfoSection">
+      <div className="BasicInfoSection-div">
+        {/* <h2>Basic Info</h2> */}
+        <p>
+          <a href="#">coffeeshop.com</a>
+          <i class="fas fa-external-link-alt"></i>
+        </p>
+
+        <hr></hr>
+
+        <p>(123) 456-7890<i class="fas fa-phone"></i></p>
+
+        <hr></hr>
+
+        <p className="BasicInfoSection-directions">
+          <a href="#">Get Directions</a>
+          <i class="fas fa-directions"></i>
+        </p>
+
+        <p className="BasicInfoSection-address">123 Fake Street Oakland, CA 12345</p>
+
+        <hr></hr>
+
+        <p>
+          <a href="#">Message the Business</a>
+          <i class="far fa-comment-dots"></i>
+        </p>
       </div>
     </section>
   );
