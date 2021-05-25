3d91e8202289ca1d191cb32e2576fe4c729a6158
gcmartin <gscalica@gmail.com>
2021-05-25 15:58:08 -0700

    Style tiles in MenuSection

--- src/components/MenuSection/index.js
+++ src/components/MenuSection/index.js
@@ -1,37 +1,50 @@
 import React from "react";
 import CategoryTile from "./../CategoryTile";
+import "./styles.css";
 
 function MenuSection() {
   return (
-    <div>
+    <section className="MenuSection">
       <h1>Menu</h1>
-      <div>
-        {/* Image by Fahmi Fakhrudin: https://unsplash.com/@fahmipaping */}
-        <CategoryTile 
-          src="https://images.unsplash.com/photo-1541167760496-1628856ab772?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3588&q=80"
-          alt="Latte"
-          category="Latte"
-        />
+      <div className="MenuSection-tile">
+        <a href="#">
+          {/* Image by Briana Tozour: https://unsplash.com/@britozour */}
+          <CategoryTile 
+            src="https://images.unsplash.com/photo-1561395049-69684aacf8de?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80"
+            alt="Latte"
+            category="Latte"
+            section="MenuSection"
+          />
+          <p>2 Photos &#8226; 2 Reviews</p>
+        </a> 
       </div>
 
-      <div>
-        {/* Image by Jocelyn Morales: https://unsplash.com/@molnj */}
-        <CategoryTile 
-          src="https://images.unsplash.com/photo-1599940778173-e276d4acb2bb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3737&q=80"
-          alt="Croissant"
-          category="Croissant"
-        />
+      <div className="MenuSection-tile">
+        <a href="#">
+          {/* Image by Jocelyn Morales: https://unsplash.com/@molnj */}
+          <CategoryTile 
+            src="https://images.unsplash.com/photo-1599940778173-e276d4acb2bb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3737&q=80"
+            alt="Croissant"
+            category="Croissant"
+            section="MenuSection"
+          />
+          <p>2 Photos &#8226; 2 Reviews</p>
+        </a>
       </div>
 
-      <div>
-        {/* Image by Joe Smith: https://unsplash.com/@joepix */}
-        <CategoryTile 
-          src="https://images.unsplash.com/photo-1590671886400-8f8088b97cb0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3445&q=80"
-          alt="Cookie"
-          category="Cookie"
-        />
+      <div className="MenuSection-tile">
+        <a href="#">
+          {/* Image by Joe Smith: https://unsplash.com/@joepix */}
+          <CategoryTile 
+            src="https://images.unsplash.com/photo-1590671886400-8f8088b97cb0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3445&q=80"
+            alt="Cookie"
+            category="Cookie"
+            section="MenuSection"
+          />
+          <p>2 Photos &#8226; 2 Reviews</p>
+        </a>
       </div>
-    </div>
+    </section>
   );
 }
 
