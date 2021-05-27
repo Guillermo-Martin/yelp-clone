6c4e5490bf8fd5d983f3845460f8dc25c73f5d0b
gcmartin <gscalica@gmail.com>
2021-05-27 16:23:54 -0700

    Style paragraph, add margin-left to photo/check-in icons, resize picture, remove UserReview left padding

--- src/components/UserReview/index.js
+++ src/components/UserReview/index.js
@@ -39,14 +39,17 @@ function UserReview() {
         <span><i class="far fa-check-circle"></i>8 check-ins</span>
       </div>
 
-      {/* // review */}
-      <p>
+      {/* Review */}
+      <p className="UserReview-text">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
       </p>
 
-      {/* // pictures user took */}
+      {/* Pictures user took */}
       {/* Picture by RR Abrot: https://unsplash.com/@rr_abrot */}
-      <img src="https://images.unsplash.com/photo-1542181961-9590d0c79dab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80" alt="coffee shop" className="UserReview-user-img"/>
+      <div className="UserReview-images">
+        <img src="https://images.unsplash.com/photo-1542181961-9590d0c79dab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80" alt="coffee shop" className="UserReview-share-img"/>
+      </div>
+      
 
       {/* // row of buttons for "useful", "funny", "cool" */}
       <div>
