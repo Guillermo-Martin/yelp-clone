8f668e7f4f705d44cc4d104786c8a3b9ec17ef46
gcmartin <gscalica@gmail.com>
2021-05-28 09:51:36 -0700

    Edit content in LogInPage component

--- src/components/LogInPage/index.js
+++ src/components/LogInPage/index.js
@@ -4,27 +4,29 @@ import { Button, Form } from "react-bootstrap";
 function LogInPage() {
   return (
     <div>
-      <h1>Log In Page</h1>
-      <Button>Continue with Facebook</Button>
-      <Button>Continue with Google</Button>
-      <Button>Continue with Apple</Button>
-      <p>or</p>
+      {/* Intro text */}
+      <div>
+        <p>Log in to Explore</p>
+        <p>New to Explore? <a href="#">Sign up</a></p>
+        <p>By logging in, you agree to Explore’s <a href="#">Terms of Service</a> and <a href="#">Privacy Policy.</a></p>
+      </div>
+      <Button><i class="fab fa-facebook"></i>Continue with Facebook</Button>
+      <Button><i class="fab fa-google"></i>Continue with Google</Button>
+      <Button><i class="fab fa-apple"></i>Continue with Apple</Button>
+      <hr/>or <hr/>
 
       {/* Form */}
       <Form>
         <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Email" />
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" />
+          <Form.Label><a href="#">Forgot password?</a></Form.Label>
         </Form.Group>
         
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
+        <Button variant="primary" type="submit">Log In</Button>
       </Form>
 
     </div>
