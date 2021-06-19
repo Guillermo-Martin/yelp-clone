055cf6602a556930afd425f4ec5de40deaecc1f2
gcmartin <gscalica@gmail.com>
2021-06-19 16:28:49 -0700

    Create styles.css for SearchResultsCard and SearchResultsPage and style card image and title

--- client/src/components/SearchResultsCard/index.js
+++ client/src/components/SearchResultsCard/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import reviewStars from "./../../assets/images/reviewStars.png";
+import "./styles.css";
 
 function SearchResultsCard() {
   return (
-    <div>
-      <Card>
+      <Card className="SearchResultsCard">
         
         <Card.Body>
-          <img src="https://images.unsplash.com/photo-1534308983496-4fabb1a015ee?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3455&q=80" alt="pizza" />
-          <Card.Title>Special title treatment</Card.Title>
+          <img className="SearchResultsCard-img"src="https://images.unsplash.com/photo-1534308983496-4fabb1a015ee?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3455&q=80" alt="pizza" />
+          <Card.Title className="SearchResultsCard-business">Pizza Restaurant</Card.Title>
           <img src={reviewStars} alt="review stars" />
           <p>$$ &#8226; Pizza</p>
           <Card.Text>
@@ -19,7 +19,6 @@ function SearchResultsCard() {
         </Card.Body>
       </Card>
 
-    </div>
   );
 }
 
--- client/src/components/SearchResultsPage/index.js
+++ client/src/components/SearchResultsPage/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import SearchResultsCard from "./../SearchResultsCard";
+import "./styles.css";
 
 function SearchResultsPage() {
   return (
     <div>
-      <h2>Best Pizza near Oakland, CA</h2>
-      <h3>All Results</h3>
+      {/* <h2>Best Pizza near Oakland, CA</h2>
+      <h3>All Results</h3> */}
       {/* Put search results here */}
 
       <SearchResultsCard />
-      
+
     </div>
   );
 }
