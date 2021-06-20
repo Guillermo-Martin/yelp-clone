3429dedeb5867613f48c8f4ee45859a1d6c31e37
gcmartin <gscalica@gmail.com>
2021-06-19 17:12:14 -0700

    Restructure SearchResultsCard and style text

--- client/src/components/SearchResultsCard/index.js
+++ client/src/components/SearchResultsCard/index.js
@@ -1,23 +1,32 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import reviewStars from "./../../assets/images/reviewStars.png";
 import "./styles.css";
 
 function SearchResultsCard() {
   return (
-      <Card className="SearchResultsCard">
-        
-        <Card.Body>
-          <img className="SearchResultsCard-img"src="https://images.unsplash.com/photo-1534308983496-4fabb1a015ee?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3455&q=80" alt="pizza" />
+    <Card className="SearchResultsCard">
+      <Link to="#"><Card.Img variant="top" src="https://images.unsplash.com/photo-1534308983496-4fabb1a015ee?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3455&q=80" /></Link>
+      <Card.Body>
+        <div className="SearchResultsCard-info">
           <Card.Title className="SearchResultsCard-business">Pizza Restaurant</Card.Title>
-          <img src={reviewStars} alt="review stars" />
-          <p>$$ &#8226; Pizza</p>
-          <Card.Text>
-          Cursus sit amet dictum sit amet justo donec. Nunc eget lorem dolor sed. Praesent semper feugiat nibh sed pulvinar proin.
-          </Card.Text>
-          
-        </Card.Body>
-      </Card>
+          <div className="SearchResultsCard-text">
+            <div>123 Fake Street</div>
+            <div>Oakland, CA 12345</div>
+          </div>
+        </div>
+        
+        <Card.Text className="SearchResultsCard-review SearchResultsCard-text"><img className="SearchResultsCard-stars"src={reviewStars} alt="review" />22</Card.Text>
+        <Card.Text className="SearchResultsCard-text">$$ &#8226; Pizza</Card.Text>
+        <div>
+          <p>Delivery</p>
+          <p>Takeout</p>
+        </div>
+        <Card.Text className="SearchResultsCard-text">Cursus sit amet dictum sit amet justo donec. Nunc eget lorem dolor sed. Praesent semper feugiat nibh sed pulvinar proin.</Card.Text>
+        <Card.Text className="SearchResultsCard-text"></Card.Text>
+      </Card.Body>
+    </Card>
 
   );
 }
