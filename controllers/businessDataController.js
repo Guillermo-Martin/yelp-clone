4c26945e53fb75816e14c1610b247aa26ec1b432
gcmartin <gscalica@gmail.com>
2021-06-22 15:53:17 -0700

    Create the /api/businessData/search API route and controller

--- controllers/businessDataController.js
+++ controllers/businessDataController.js
@@ -4,5 +4,9 @@ module.exports = {
   getAllBusinessData: (req, res) => {
     console.log("you've reached the getAllBusinessData function!");
     return res.json(businessData);
+  },
+  searchAllBusinessData: (req, res) => {
+    console.log("you've reached the searchAllBusinessData function!")
+    res.send("You've reached the searchAllBusinessData function!")
   }
 }
--- routes/apiRoutes/businessDataRoutes/index.js
+++ routes/apiRoutes/businessDataRoutes/index.js
@@ -1,10 +1,14 @@
 const router = require("express").Router();
 
 // Functions from the "businessDataController"
-const { getAllBusinessData } = require("./../../../controllers/businessDataController");
+const { getAllBusinessData, searchAllBusinessData } = require("./../../../controllers/businessDataController");
 
 // Route reached here:  "/api/businessData"
 router.route("/")
-  .get(getAllBusinessData)
- 
+  .get(getAllBusinessData);
+
+// Route reached here:  "/api/businessData/search"
+router.get("/search", searchAllBusinessData);
+
+
 module.exports = router;
