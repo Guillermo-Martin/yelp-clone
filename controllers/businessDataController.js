6120d77bb39964091ed5687f0466bcc995bf4822
gcmartin <gscalica@gmail.com>
2021-06-30 15:53:12 -0700

    Put code in try/catch block, add code to searchAllBusinessData function to return results based on cityInput

--- controllers/businessDataController.js
+++ controllers/businessDataController.js
@@ -38,98 +38,98 @@ module.exports = {
     const { businessInput, cityInput } = req.params;
     console.log(businessInput, cityInput);
 
-    // if both inputs are "default" (meaning, there were no inputs), return all the data
-    if(businessInput === "default" && cityInput === "default"){
-      console.log("Inputs are empty!");
+    // create a collection reference (for database searching)
+    const businessRef = db.collection("businesses");
 
-      // create a collection reference
-      const businessRef = db.collection("businesses");
+    try {
+      // if both inputs are "default" (meaning, there were no inputs), return all the data
+      if(businessInput === "default" && cityInput === "default"){
+        console.log("Inputs are empty!");
 
-      // create a query snapshot
-      const querySnapshot = await businessRef.get();
+        // create a query snapshot
+        const querySnapshot = await businessRef.get();
 
-      // access querySnapshot as an array (using ".docs"), map through it.  For every document, return its data
-      let allBusinessData = querySnapshot.docs.map(doc => {
-        return doc.data();
-      });
+        // access querySnapshot as an array (using ".docs"), map through it.  For every document, return its data
+        let allBusinessData = querySnapshot.docs.map(doc => {
+          return doc.data();
+        });
 
-      // send data to front end
-      res.json(allBusinessData);
+        // send data to front end
+        return res.status(200).json(allBusinessData);
 
-    } else if(businessInput === "default"){
-      // if businessInput is empty, search through all the cities
-      // filter through businessData JSON
-      // let allSearchResults = businessData.filter(function(business){
-
+      } else if(businessInput === "default"){
+        // if businessInput is "default" (meaning businessInput was empty), search through all the cities
         // change cityInput into a single string and lowercase
-        // let cityInputLower = cityInput.split(" ").join("").toLowerCase();
+        let cityInputLower = cityInput.split(" ").join("").toLowerCase();
 
-        // change "neighborhood" in businessData JSON to lowercase
-        // let neighborhoodLower = business.neighborhood.split(" ").join("").toLowerCase();
+        // create a query snapshot, see if "cityInputLower" is found in the "queryCity" array of each document
+        const querySnapshot = await businessRef.where("queryCity", "array-contains", cityInputLower).get();
+
+        // access querySnapshot as an array, map, return data
+        let allSearchResults = querySnapshot.docs.map(doc => {
+          return doc.data();
+        });
+
+        // send data to front end
+        return res.status(200).json(allSearchResults);
         
-        // see if the "cityInput" exists in the "queryCity" array or in "neighborhood"
-        // if(business.queryCity.includes(cityInputLower) || neighborhoodLower === cityInputLower){
-        //   // if it does, add it to allSearchResults;
-        //   return business;
-        // }
-      // });
- 
-      // // send the filtered results back to the front end
-      // console.log("allSearchResults", allSearchResults);
-      // return res.json(allSearchResults);
-    } else if(cityInput === "default") {
-      // filter through businessData JSON
-      // let allSearchResults = businessData.filter(function(business){
+      } else if(cityInput === "default") {
+        // filter through businessData JSON
+        // let allSearchResults = businessData.filter(function(business){
 
-      //   // change businessInput, business "type" to lowercase
-      //   let businessInputLower = businessInput.toLowerCase();
-        
-      //   // change business "name" to lowercase letters, then turn into an array
-      //   let businessNameArr = business.name.toLowerCase().split(" ");
+        //   // change businessInput, business "type" to lowercase
+        //   let businessInputLower = businessInput.toLowerCase();
+          
+        //   // change business "name" to lowercase letters, then turn into an array
+        //   let businessNameArr = business.name.toLowerCase().split(" ");
 
-      //   // change all "types" in the array to lowercase
-      //   let businessTypesLower = business.type.map(type => type.toLowerCase());
+        //   // change all "types" in the array to lowercase
+        //   let businessTypesLower = business.type.map(type => type.toLowerCase());
 
-      //   // see if the "businessInput" exists in the "name" or "type" array
-      //   if(businessNameArr.includes(businessInputLower) || businessTypesLower.includes(businessInputLower)){
-      //     // if it does, add it to allSearchResults;
-      //     return business;
-      //   }
-      // });
+        //   // see if the "businessInput" exists in the "name" or "type" array
+        //   if(businessNameArr.includes(businessInputLower) || businessTypesLower.includes(businessInputLower)){
+        //     // if it does, add it to allSearchResults;
+        //     return business;
+        //   }
+        // });
 
-      // // send the filtered results back to the front end
-      // console.log("allSearchResults", allSearchResults);
-      // return res.json(allSearchResults);
-    } else {
-      // filter through businessData JSON
-      // let allSearchResults = businessData.filter(function(business){
+        // // send the filtered results back to the front end
+        // console.log("allSearchResults", allSearchResults);
+        // return res.json(allSearchResults);
+      } else {
+        // filter through businessData JSON
+        // let allSearchResults = businessData.filter(function(business){
 
-      //   // change businessInput, business "type" to lowercase
-      //   let businessInputLower = businessInput.toLowerCase();
-        
-      //   // change business "name" to lowercase letters, then turn into an array
-      //   let businessNameArr = business.name.toLowerCase().split(" ");
+        //   // change businessInput, business "type" to lowercase
+        //   let businessInputLower = businessInput.toLowerCase();
+          
+        //   // change business "name" to lowercase letters, then turn into an array
+        //   let businessNameArr = business.name.toLowerCase().split(" ");
 
-      //   // change all "types" in the array to lowercase
-      //   let businessTypesLower = business.type.map(type => type.toLowerCase());
+        //   // change all "types" in the array to lowercase
+        //   let businessTypesLower = business.type.map(type => type.toLowerCase());
 
-      //   // change cityInput into a single string and lowercase
-      //   let cityInputLower = cityInput.split(" ").join("").toLowerCase();
+        //   // change cityInput into a single string and lowercase
+        //   let cityInputLower = cityInput.split(" ").join("").toLowerCase();
 
-      //   // change "neighborhood" in businessData JSON to lowercase
-      //   let neighborhoodLower = business.neighborhood.split(" ").join("").toLowerCase();
-        
-      //   // see if the "businessInput" and "cityInput" exits in the "name", "type", "queryCity", or "neighborhood"
-      //   if((businessNameArr.includes(businessInputLower) || businessTypesLower.includes(businessInputLower)) && (business.queryCity.includes(cityInputLower) || neighborhoodLower === cityInputLower)){
-      //     // if it does, add it to allSearchResults;
-      //     return business;
-      //   }
-      // });
+        //   // change "neighborhood" in businessData JSON to lowercase
+        //   let neighborhoodLower = business.neighborhood.split(" ").join("").toLowerCase();
+          
+        //   // see if the "businessInput" and "cityInput" exits in the "name", "type", "queryCity", or "neighborhood"
+        //   if((businessNameArr.includes(businessInputLower) || businessTypesLower.includes(businessInputLower)) && (business.queryCity.includes(cityInputLower) || neighborhoodLower === cityInputLower)){
+        //     // if it does, add it to allSearchResults;
+        //     return business;
+        //   }
+        // });
 
-      // // send the filtered results back to the front end
-      // console.log("allSearchResults", allSearchResults);
-      // return res.json(allSearchResults);
+        // // send the filtered results back to the front end
+        // console.log("allSearchResults", allSearchResults);
+        // return res.json(allSearchResults);
+      }
+    } catch (error) {
+      return res.status(404).json(error);
     }
+    
   }
 }
 
