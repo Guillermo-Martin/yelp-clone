05dadaa13d35d034c37dc97de3a074c9e4f2527e
gcmartin <gscalica@gmail.com>
2021-07-10 20:08:25 -0700

    Delete businessData.json, create 'data' folder and 'cities.js', add content and style

---
+++ client/src/assets/data/cities.js
@@ -0,0 +1,158 @@
+ // East Bay
+ export const eastBay = [
+  {
+    city: "Alameda",
+    map: "https://www.google.com/maps/place/Alameda,+CA/@37.7856596,-122.2879047,13z/data=!4m5!3m4!1s0x808f80d8f2cf1595:0x66ff99cf60016f14!8m2!3d37.7798721!4d-122.2821855"
+  },
+  {
+    city: "Berkeley",
+    map: "https://www.google.com/maps/place/Berkeley,+CA/@37.8712261,-122.3364571,13z/data=!3m1!4b1!4m5!3m4!1s0x808579363a8549d3:0x94ea1595a675e993!8m2!3d37.8715226!4d-122.273042"
+  },
+  {
+    city: "Emeryville",
+    map: "https://www.google.com/maps/place/Emeryville,+CA/@37.8385092,-122.3223696,14z/data=!3m1!4b1!4m5!3m4!1s0x80857e14ea1f8eb3:0x481d1831a8b07107!8m2!3d37.8395028!4d-122.2892278"
+  },
+  {
+    city: "Fremont",
+    map: "https://www.google.com/maps/place/Fremont,+CA/@37.5305464,-122.0818585,12z/data=!3m1!4b1!4m5!3m4!1s0x808fbf46b7e8caf7:0x8ada313b89d888d4!8m2!3d37.5485396!4d-121.988583"
+  },
+  {
+    city: "Hayward",
+    map: "https://www.google.com/maps/place/Hayward,+CA/@37.6182289,-122.1393517,12z/data=!3m1!4b1!4m5!3m4!1s0x808f9431b601597d:0x70474dec26939c0!8m2!3d37.6687665!4d-122.0809964"
+  },
+  {
+    city: "Lafayette",
+    map: "https://www.google.com/maps/place/Lafayette,+CA/@37.8937865,-122.1578774,13z/data=!3m1!4b1!4m5!3m4!1s0x8085607ca3fa8f85:0xf928585d03e46927!8m2!3d37.8857582!4d-122.1180201"
+  },
+  {
+    city: "Oakland",
+    map: "https://www.google.com/maps/place/Oakland,+CA/@37.7588325,-122.3053475,12z/data=!3m1!4b1!4m5!3m4!1s0x80857d8b28aaed03:0x71b415d535759367!8m2!3d37.8043514!4d-122.2711639"
+  },
+  {
+    city: "Piedmont",
+    map: "https://www.google.com/maps/place/Piedmont,+CA/@37.8227404,-122.2386354,15z/data=!3m1!4b1!4m5!3m4!1s0x80857d7f7f19c5f5:0xb8deddc8c24bd3f!8m2!3d37.8244063!4d-122.2314865"
+  },
+  {
+    city: "San Leandro",
+    map: "https://www.google.com/maps/place/San+Leandro,+CA/@37.7051459,-122.2011704,13z/data=!3m1!4b1!4m5!3m4!1s0x808f857f043a66dd:0xe5aa490ddad757b2!8m2!3d37.7257663!4d-122.1568554"
+  },
+  {
+    city: "Walnut Creek",
+    map: "https://www.google.com/maps/place/Walnut+Creek,+CA/@37.894286,-122.0752423,13z/data=!3m1!4b1!4m5!3m4!1s0x808ff474197ba5f3:0xe06e49972003c36c!8m2!3d37.9100783!4d-122.0651819"
+  }
+];
+
+// San Francisco
+export const sanFrancisco = [
+  {
+    city: "Castro",
+    map: "https://www.google.com/maps/place/The+Castro,+San+Francisco,+CA/@37.7625332,-122.440545,16z/data=!3m1!4b1!4m5!3m4!1s0x808f7e1b3606d49d:0xc9a66606d5327c89!8m2!3d37.7609082!4d-122.4350043"
+  },
+  {
+    city: "Embarcadero",
+    map: "https://www.google.com/maps/place/Embarcadero,+San+Francisco,+CA/@37.7947087,-122.4144961,14z/data=!3m1!4b1!4m5!3m4!1s0x80858066993643ad:0x77178399d59a1e7f!8m2!3d37.7992627!4d-122.3976732"
+  },
+  {
+    city: "Financial District",
+    map: "https://www.google.com/maps/place/Financial+District,+San+Francisco,+CA/@37.7927815,-122.4054696,16z/data=!3m1!4b1!4m5!3m4!1s0x80858060404835cd:0xb970d02d5747a77c!8m2!3d37.7945742!4d-122.3999445"
+  },
+  {
+    city: "Marina District",
+    map: "https://www.google.com/maps/place/Marina+District,+San+Francisco,+CA/@37.8038433,-122.4418518,16z/data=!3m1!4b1!4m5!3m4!1s0x808580d79ae5a51b:0x3c50fe8f3930832!8m2!3d37.8036667!4d-122.4368151"
+  },
+  {
+    city: "Mission District",
+    map: "https://www.google.com/maps/place/Mission+District,+San+Francisco,+CA/@37.7599213,-122.4256016,15z/data=!3m1!4b1!4m5!3m4!1s0x808f7e3c5f803323:0x35800073de45189!8m2!3d37.7598648!4d-122.4147977"
+  },
+  {
+    city: "Nob Hill",
+    map: "https://www.google.com/maps/place/Nob+Hill,+San+Francisco,+CA/@37.7929521,-122.4204894,16z/data=!3m1!4b1!4m5!3m4!1s0x808580ed1dac251f:0xb4797f7e513809c0!8m2!3d37.793014!4d-122.416113"
+  },
+  {
+    city: "Noe Valley",
+    map: "https://www.google.com/maps/place/Noe+Valley,+San+Francisco,+CA/@37.749064,-122.4388547,16z/data=!3m1!4b1!4m5!3m4!1s0x808f7e132a3d1743:0xc423d238ea7005ea!8m2!3d37.7502378!4d-122.4337029"
+  },
+  {
+    city: "North Beach",
+    map: "https://www.google.com/maps/place/North+Beach,+San+Francisco,+CA/@37.8047205,-122.4125737,16z/data=!3m1!4b1!4m5!3m4!1s0x808580f0490719ad:0x7fc167504a989706!8m2!3d37.8060532!4d-122.4103311"
+  },
+  {
+    city: "Pacific Heights",
+    map: "https://www.google.com/maps/place/Pacific+Heights,+San+Francisco,+CA/@37.7924366,-122.4393719,16z/data=!3m1!4b1!4m5!3m4!1s0x808580c593750c47:0x26f06ef70d70f47c!8m2!3d37.7925153!4d-122.4382307"
+  },
+  {
+    city: "Telegraph Hill",
+    map: "https://www.google.com/maps/place/Telegraph+Hill,+San+Francisco,+CA/@37.801671,-122.4083872,17z/data=!3m1!4b1!4m5!3m4!1s0x808580f6d1d7055f:0x8699b92ae52dae84!8m2!3d37.8013407!4d-122.4056674"
+  }
+];
+
+export const peninsula = [
+  {
+    city: "Burlingame",
+    map: "https://www.google.com/maps/place/Burlingame,+CA/@37.5877683,-122.3849216,14z/data=!3m1!4b1!4m5!3m4!1s0x808f7625549b8e59:0xbbcfa3e532f8f4f!8m2!3d37.5778696!4d-122.34809"
+  },
+  {
+    city: "Daly City",
+    map: "https://www.google.com/maps/place/Daly+City,+CA/@37.6787152,-122.4704281,14z/data=!3m1!4b1!4m5!3m4!1s0x808f7bfb84de63c9:0x2cb4e0db0e13fa57!8m2!3d37.6879241!4d-122.4702079"
+  },
+  {
+    city: "Menlo Park",
+    map: "https://www.google.com/maps/place/Menlo+Park,+CA/@37.4764681,-122.1900944,13z/data=!3m1!4b1!4m5!3m4!1s0x808fa6b1117280ff:0xebbf998e5df289ab!8m2!3d37.4529598!4d-122.1817252"
+  },
+  {
+    city: "Palo Alto",
+    map: "https://www.google.com/maps/place/Palo+Alto,+CA/@37.425713,-122.1703694,13z/data=!3m1!4b1!4m5!3m4!1s0x808fb07b9dba1c39:0xe1ff55235f576cf!8m2!3d37.4418834!4d-122.1430195"
+  },
+  {
+    city: "Redwood City",
+    map: "https://www.google.com/maps/place/Redwood+City,+CA/@37.5084363,-122.2506038,13z/data=!3m1!4b1!4m5!3m4!1s0x808fa03953f35ee1:0xb8e47b9b0baa6a6d!8m2!3d37.4847955!4d-122.228141"
+  },
+  {
+    city: "San Mateo",
+    map: "https://www.google.com/maps/place/San+Mateo,+CA/@37.5565988,-122.3506494,13z/data=!3m1!4b1!4m5!3m4!1s0x808f9e60efa95545:0xfd8efcf42dcc1ba7!8m2!3d37.5629917!4d-122.3255254"
+  }
+];
+
+// South Bay
+export const southBay = [
+  {
+    city: "Cupertino",
+    map: "https://www.google.com/maps/place/Cupertino,+CA/@37.3092828,-122.061154,14z/data=!3m1!4b1!4m5!3m4!1s0x808fb4571bd377ab:0x394d3fe1a3e178b4!8m2!3d37.3229978!4d-122.0321823"
+  },
+  {
+    city: "Mountain View",
+    map: "https://www.google.com/maps/place/Mountain+View,+CA/@37.4133981,-122.1162864,13z/data=!3m1!4b1!4m5!3m4!1s0x808fb7495bec0189:0x7c17d44a466baf9b!8m2!3d37.3860517!4d-122.0838511"
+  },
+  {
+    city: "San Jose",
+    map: "https://www.google.com/maps/place/San+Jose,+CA/@37.2970523,-121.9574959,11z/data=!3m1!4b1!4m5!3m4!1s0x808fcae48af93ff5:0xb99d8c0aca9f717b!8m2!3d37.3382082!4d-121.8863286"
+  },
+  {
+    city: "Santa Clara",
+    map: "https://www.google.com/maps/place/Santa+Clara,+CA/@37.3709535,-122.0025719,13z/data=!3m1!4b1!4m5!3m4!1s0x808fb7815c08c193:0xe475a47ca3c0bfc0!8m2!3d37.3541079!4d-121.9552356"
+  },
+  {
+    city: "Sunnyvale",
+    map: "https://www.google.com/maps/place/Sunnyvale,+CA/@37.3958499,-122.0939077,12z/data=!3m1!4b1!4m5!3m4!1s0x808fb645a9d05d3b:0x768dfb26dd7cc3a2!8m2!3d37.36883!4d-122.0363496"
+  }
+];
+
+// South Bay
+export const northBay = [
+  {
+    city: "Napa",
+    map: "https://www.google.com/maps/place/Napa,+CA/@38.2860765,-122.3599981,12z/data=!3m1!4b1!4m5!3m4!1s0x8084ffe7f8f2deef:0xd6629f3a3384c725!8m2!3d38.2975381!4d-122.286865"
+  },
+  {
+    city: "Mill Valley",
+    map: "https://www.google.com/maps/place/Mill+Valley,+CA+94941/@37.9067306,-122.5631826,14z/data=!3m1!4b1!4m5!3m4!1s0x808590733c5a3c93:0xd27fa4ddcff9d586!8m2!3d37.9060368!4d-122.5449763"
+  },
+  {
+    city: "Sausalito",
+    map: "https://www.google.com/maps/place/Sausalito,+CA+94965/@37.8584346,-122.5023378,15z/data=!3m1!4b1!4m5!3m4!1s0x8085845331c92675:0xb903aa6d828f51ee!8m2!3d37.8590937!4d-122.4852507"
+  },
+  {
+    city: "Tiburon",
+    map: "https://www.google.com/maps/place/Tiburon,+CA/@37.8817385,-122.4982935,13z/data=!3m1!4b1!4m5!3m4!1s0x8085849a60255613:0x4f93d66dafe61acb!8m2!3d37.8735377!4d-122.4566392"
+  },
+];
--- client/src/components/MoreCitiesPage/index.js
+++ client/src/components/MoreCitiesPage/index.js
@@ -1,8 +1,75 @@
 import React from "react";
+import { eastBay, sanFrancisco, peninsula, southBay, northBay } from "./../../assets/data/cities";
+import "./styles.css";
+
+// create li's for each region
+const eastBayCities = eastBay.map(city => (
+  <li className="MoreCitiesPage-li"><a href={city.map}>{city.city}</a></li>
+));
+
+const sfNeighborhoods = sanFrancisco.map(neighborhood => (
+  <li className="MoreCitiesPage-li"><a href={neighborhood.map}>{neighborhood.city}</a></li>
+));
+
+const peninsulaCities = peninsula.map(city => (
+  <li className="MoreCitiesPage-li"><a href={city.map}>{city.city}</a></li>
+));
+
+const southBayCities = southBay.map(city => (
+  <li className="MoreCitiesPage-li"><a href={city.map}>{city.city}</a></li>
+));
+
+const northBayCities = northBay.map(city => (
+  <li className="MoreCitiesPage-li"><a href={city.map}>{city.city}</a></li>
+));
+
+
 
 function MoreCitiesPage() {
   return (
-    <h1>More Cities Page!</h1>
+    <section className="MoreCitiesPage">
+      <h2>More Places to Explore</h2>
+
+      <hr></hr>
+      
+      <div className="MoreCitiesPage-cities">
+        <div className="MoreCitiesPage-cities-float">
+          <h3>East Bay</h3>
+          <ul className="MoreCitiesPage-ul">
+            {eastBayCities}
+          </ul>
+        </div>
+        
+        <div className="MoreCitiesPage-cities-float">
+          <h3>San Francisco</h3>
+          <ul className="MoreCitiesPage-ul">
+            {sfNeighborhoods}
+          </ul>
+        </div>
+
+        <div className="MoreCitiesPage-cities-float">
+          <h3>Peninsula</h3>
+          <ul className="MoreCitiesPage-ul">
+            {peninsulaCities}
+          </ul>
+        </div>
+
+        <div className="MoreCitiesPage-cities-float">
+          <h3>South Bay</h3>
+          <ul className="MoreCitiesPage-ul">
+            {southBayCities}
+          </ul>
+        </div>
+
+        <div className="MoreCitiesPage-cities-last">
+          <h3>North Bay</h3>
+          <ul className="MoreCitiesPage-ul">
+            {northBayCities}
+          </ul>
+        </div>
+      </div>
+      
+    </section>
   );
 }
 
