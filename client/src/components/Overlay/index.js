668a5dcaf079481d51b03920578ed7462981ca24
gcmartin <gscalica@gmail.com>
2021-07-22 11:41:05 -0700

    Style and position overlay

--- client/src/components/Overlay/index.js
+++ client/src/components/Overlay/index.js
@@ -4,8 +4,12 @@ import "./styles.css";
 function Overlay(){
   return (
     <div className="Overlay">
-      <h1>I'm an overlay!</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <div className="Overlay-div">
+        <img src="https://images.unsplash.com/photo-1577805947697-89e18249d767?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3382&q=80" alt="milkshake" 
+        className="Overlay-img"
+        />
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+      </div> 
     </div>
   );
 }
