88a8719a78c8cf2d95a8d63404abababbf33dc22
gcmartin <gscalica@gmail.com>
2021-07-27 15:27:28 -0700

    Create MenuPage component and add basic content

---
+++ client/src/components/MenuPage/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function MenuPage() {
+  return (
+    <div>
+      <h1>Menu For Business</h1>
+      {/* Menu items will go below */}
+      <div>
+        <h4>Item</h4>
+        <p>Description</p>
+        <div>Price</div>
+      </div>
+    </div>
+  );
+}
+
+export default MenuPage;
