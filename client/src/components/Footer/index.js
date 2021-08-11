2e019d81b9786e4dfcf36cbe26e8ec01e2849ee1
gcmartin <gscalica@gmail.com>
2021-08-11 16:54:04 -0700

    Add 'Discover' section in footer and adjust positioning of elements

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,14 +5,28 @@ import "./styles.css";
 function Footer(){
   return (
     <footer className="Footer">
-      <h6 className="Footer-section-header">About</h6>
-      <ul className="Footer-section-ul">
-        <li><a href="#">lorem ispum</a></li>
-        <li><a href="#">dolor sit amet</a></li>
-        <li><a href="#">consectetur</a></li>
-        <li><a href="#">magna aliqua</a></li>
-        <li><a href="#">reprehenderit</a></li>
-      </ul>
+      <div className="Footer-col">
+        <h6 className="Footer-section-header">About</h6>
+        <ul className="Footer-section-ul">
+          <li><a href="#">lorem ispum</a></li>
+          <li><a href="#">dolor sit amet</a></li>
+          <li><a href="#">consectetur</a></li>
+          <li><a href="#">magna aliqua</a></li>
+          <li><a href="#">reprehenderit</a></li>
+        </ul>
+      </div>
+
+      <div className="Footer-col">
+        <h6 className="Footer-section-header">Discover</h6>
+        <ul className="Footer-section-ul">
+          <li><a href="#">lorem ispum</a></li>
+          <li><a href="#">dolor sit amet</a></li>
+          <li><a href="#">consectetur</a></li>
+          <li><a href="#">magna aliqua</a></li>
+          <li><a href="#">reprehenderit</a></li>
+        </ul>
+      </div>
+      
       <img src={footerLogo} alt="Footer logo" />
     </footer>
   );
